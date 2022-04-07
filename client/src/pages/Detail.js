bbcfdec89260d6d9fe8e26ccc3d6e15b7342fc96
Kellie Harman <knharman@gmail.com>
2022-04-07 15:41:27 -0700

    adding items to cart implemented

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -6,8 +6,14 @@ import { QUERY_PRODUCTS } from '../utils/queries';
 import spinner from '../assets/spinner.gif';
 
 import { useStoreContext } from "../utils/GlobalState";
-import { UPDATE_PRODUCTS } from "../utils/actions";
 import Cart from '../components/Cart';
+
+import {
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  ADD_TO_CART,
+  UPDATE_PRODUCTS,
+} from '../utils/actions';
 
 function Detail() {
   const [state, dispatch] = useStoreContext();
@@ -18,6 +24,12 @@ function Detail() {
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
   const { products } = state;
+  const addToCart = () => {
+    dispatch({
+      type: ADD_TO_CART,
+      product: { ...currentProduct, purchaseQuantity: 1 }
+    });
+  };
 
   useEffect(() => {
     if (products.length) {
@@ -42,14 +54,14 @@ function Detail() {
 
           <p>
             <strong>Price:</strong>${currentProduct.price}{' '}
-            <button>Add to Cart</button>
+            <button onClick={addToCart}>Add to Cart</button>
             <button>Remove from Cart</button>
           </p>
 
           <img
             src={`/images/${currentProduct.image}`}
             alt={currentProduct.name}
-         />
+          />
         </div>
       ) : null}
       {loading ? <img src={spinner} alt="loading" /> : null}
